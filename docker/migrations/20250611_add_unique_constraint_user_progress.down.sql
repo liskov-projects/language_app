c558f8c4373740e9603cfc76e0b0020c0cb46ce4
verrakav <verrafalenko@gmail.com>
2025-06-11 15:21:49 +0930

    added comments

--- docker/migrations/20250611_add_unique_constraint_user_progress.down.sql
+++ docker/migrations/20250611_add_unique_constraint_user_progress.down.sql
@@ -1,2 +1,4 @@
+-- needed if we want to roll back to prev version
+
 ALTER TABLE user_progress 
 DROP CONSTRAINT IF EXISTS user_word_unique;
--- docker/migrations/20250611_add_unique_constraint_user_progress.up.sql
+++ docker/migrations/20250611_add_unique_constraint_user_progress.up.sql
@@ -1,2 +1,5 @@
+-- most migration tools require both
+
+-- this runs, but if we want to undo the change we use .down -- 
 ALTER TABLE user_progress
 ADD CONSTRAINT user_word_unique UNIQUE (user_id, word);
